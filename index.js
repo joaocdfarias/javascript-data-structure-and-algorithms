d33a8d4142f44433335c22ccf37e032b0853405f
joaocdfarias <joaocdfariass@gmail.com>
2022-05-29 23:15:49 -0300

    3 files added

---
+++ index.js
@@ -0,0 +1,4 @@
+import { circleArea, sqaureArea } from './modules.js';
+
+circleArea(4);
+sqaureArea(6);
---
+++ modules.js
@@ -0,0 +1,9 @@
+const circleArea = (r) => {
+  console.log('The circle area is => ', 3.14 * r ** 2);
+};
+
+const sqaureArea = (s) => {
+  console.log('The square area is => ', s * s);
+};
+
+export { circleArea, sqaureArea };
